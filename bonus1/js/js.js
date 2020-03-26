0969404d5fa41af34f12086d822111090d987450
Alberto <alberto.germana@gmail.com>
2020-03-26 10:51:33 +0100

    Bonus1 completato

--- bonus1/js/js.js
+++ bonus1/js/js.js
@@ -1,7 +1,23 @@
-//Scrivi un programma che stampi i numeri da 1 a 100, 
-//ma per i multipli di 3 stampi “Fizz” al posto del numero 
-//e per i multipli di 5 stampi “Buzz”. 
-//Per i numeri che sono sia multipli di 3 che di 5 stampi” FizzBuzz”.
+// -------------------- CONSEGNA ESERCIZIO --------------------
+/* Scrivi un programma che stampi i numeri da 1 a 100, 
+ma per i multipli di 3 stampi “Fizz” al posto del numero 
+e per i multipli di 5 stampi “Buzz”. 
+Per i numeri che sono sia multipli di 3 che di 5 stampi” FizzBuzz”. */
+
+
+// -------------------- BONUS --------------------
+/* come sempre l’ex potrebbe essere risolto in più varianti, anche poco differenti a livello di codice, 
+ma che comportano ottimizzazioni e variazioni sensibili, quindi prova in altra cartella a fare altre 
+varianti e scrivi descrivendo cosa è cambiato dal precedente, cosa migliora, o semplicemente cambia e perchè; */
+
+
+
+// -------------------- ESEGUO --------------------
+
+// -------------------- SPIEGO COSA CAMBIO RISPETTO L'ESERCIZIO BASE --------------------
+
+/* Rispetto l'esercizio precedente ho dichiarato delle variabili con all'interno il testo che voglio stampare
+cosi da non scrivere il testo nel console.log */
 
 //dichiaro e valorizzo variabili che conterranno la parola da stampare quando sono divisibili per un numero
 var divTre = "Fizz";
