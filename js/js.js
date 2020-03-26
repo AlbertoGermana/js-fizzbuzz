74b6c6e33a3ae89b2846fb726b5d992ff1db2ef2
Alberto <alberto.germana@gmail.com>
2020-03-26 10:36:06 +0100

    Esercizio base completato

--- js/js.js
+++ js/js.js
@@ -7,8 +7,7 @@
 //ciclo che va da 1 a 100 e analizza ogni iterazione
 for(var i = 1; i <= 100; i++){
     if(i % 3 == 0 && i % 5 == 0 ){  // se il numero dato è divisibile per 3 e 5
-        console.log("FizzBuzz"); 
-        
+        console.log("FizzBuzz");        
     }else if(i % 3 == 0){ // se il numero dato è divisibile per 3
         console.log("Fizz"); 
     }else if(i % 5 == 0){ // se il numero dato è divisibile per 5 
@@ -16,6 +15,4 @@ for(var i = 1; i <= 100; i++){
     }else{ // se il numero non è divisibile ne per 3 ne per 5
         console.log(i);
     }
-    
-    
 }
