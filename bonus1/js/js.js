0cf16f11cd41ee024f35168f115764e0095b6009
Alberto <alberto.germana@gmail.com>
2020-03-26 11:37:02 +0100

    Bonus1 completato. Aggiunta variante While

--- bonus1/js/js.js
+++ bonus1/js/js.js
@@ -42,7 +42,7 @@ for(var i = 1; i <= 100; i++){
 // -------------------- SECONDA VARIANTE --------------------
 // -------------------- SPIEGO COSA CAMBIO RISPETTO L'ESERCIZIO BASE --------------------
 
-/*                                  */
+/* Risolvo utilizzando il doWhile */
 
 //dichiaro e valorizzo variabili che conterranno la parola da stampare quando sono divisibili per un numero
 var divTre = "Fizz";
@@ -50,7 +50,7 @@ var divCinque = "Buzz";
 var divTreCinque = divTre + divCinque;
 
 //ciclo che va da 1 a 100 e analizza ogni iterazione
-var i = 1;
+/* var i = 1;
 do {
     if(i % 3 == 0 && i % 5 == 0 ){  // se il numero dato è divisibile per 3 e 5
         console.log(divTre + divCinque);        
@@ -62,4 +62,29 @@ do {
         console.log(i);
     }
     i++
-} while (i <= 100);
+} while (i <= 100); */
+
+// -------------------- TERZA VARIANTE --------------------
+// -------------------- SPIEGO COSA CAMBIO RISPETTO L'ESERCIZIO BASE --------------------
+
+/* Risolvo utilizzando il While */
+
+//dichiaro e valorizzo variabili che conterranno la parola da stampare quando sono divisibili per un numero
+var divTre = "Fizz";
+var divCinque = "Buzz";
+var divTreCinque = divTre + divCinque;
+
+//ciclo che va da 1 a 100 e analizza ogni iterazione
+var i = 1;
+while (i <= 100) {
+    if(i % 3 == 0 && i % 5 == 0 ){  // se il numero dato è divisibile per 3 e 5
+        console.log(divTre + divCinque);        
+    }else if(i % 3 == 0){ // se il numero dato è divisibile per 3
+        console.log(divTre); 
+    }else if(i % 5 == 0){ // se il numero dato è divisibile per 5 
+        console.log(divCinque); 
+    }else{ // se il numero non è divisibile ne per 3 ne per 5
+        console.log(i);
+    }
+    i++
+} 
